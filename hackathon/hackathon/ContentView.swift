4912448623c8bde73b9a14e5b83b788091a90b57
Ananya Jain <132234981+ananyajainn@users.noreply.github.com>
2024-03-09 09:46:14 -0500

    swift base upload

---
+++ hackathon/hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hackathon
+//
+//  Created by Ananya Jain on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hackathon/hackathon/hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathonApp.swift
+//  hackathon
+//
+//  Created by Ananya Jain on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
